d961f471566faf14af821b60465c1a18eab951c9
Feifei Huang <yaqi@Feifeis-MacBook-Pro.local>
2025-11-30 15:24:00 -0800

    Created the signup and login UI in flutter.

---
+++ lib/login.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import '/components/textfield.dart';
+import '/components/logoname.dart';
+// Followed tutorial on youtube: https://youtu.be/Dh-cTQJgM-Q?si=vpDDOYUqS0LrhRcU
+
+
+
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+
+  //text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //login user in method
+  void loginUser() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const LogoName(),
+              const SizedBox(height:50),
+
+
+              Container(
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border:Border.all(
+                    color: Colors.grey.shade400,
+                    width: 1,
+                  ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+
+              child: Column(
+                children:[
+                // username text field
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Enter an Email',
+                    obscureText: false,
+                  ),
+
+                  const SizedBox(height:20),
+
+                  // password text field
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
+
+                  const SizedBox(height:10),
+
+                  // forgot password?
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    )
+                  ),
+
+                  const SizedBox(height:50),
+
+                  // Login Button
+                  GestureDetector(
+                    onTap: loginUser,
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      margin: const EdgeInsets.symmetric(horizontal: 70),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        // edit border edge roundness
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height:30),
+
+                  // Don't have an account? Sign Up Now
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account?",
+                        style: TextStyle(
+                          color: Colors.black,),
+                      ),
+                      const SizedBox(width: 4),
+
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.pushNamed(context, '/signup');
+                        },
+                        child: const Text(
+                          'Sign Up Now',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
