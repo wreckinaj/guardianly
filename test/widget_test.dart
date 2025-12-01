1a9932163b321447992f600e4cdcb01295e340c3
Feifei Huang <yaqi@Feifeis-MacBook-Pro.local>
2025-11-30 16:20:28 -0800

    Add minimal smoke test to fix CI/CD test failure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-void main() {
+import 'package:flutter_test/flutter_test.dart';
+import 'package:guardianly/main.dart';
 
+void main() {
+  testWidgets('App builds without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.byType(MyApp), findsOneWidget); // simple check
+  });
 }
